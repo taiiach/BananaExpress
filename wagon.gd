39bd5a3bb51e1ec60294766e90e62c805ce2654f
Taiiach <taiiach@outlook.com>
2025-06-23 21:18:40 +0200

    Connect wagon detector signal

--- wagon.gd
+++ wagon.gd
@@ -1,5 +1,8 @@
 extends StaticBody2D
 
+func _ready():
+    $WagonDetector.body_entered.connect(_on_WagonDetector_body_entered)
+
 func _on_WagonDetector_body_entered(body):
-	if body.name == "Player":
-		body.register_wagon(get_instance_id())
+    if body.name == "Player":
+        body.register_wagon(get_instance_id())
