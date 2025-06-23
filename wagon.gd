50c6dae2dbcabe47a7cd9c21f3779f277847b2ad
taiiach <taiiach@outlook.com>
2025-06-23 21:24:50 +0200

    Update wagon.gd

--- wagon.gd
+++ wagon.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 
 func _ready():
-    $WagonDetector.body_entered.connect(_on_WagonDetector_body_entered)
+	$WagonDetector.body_entered.connect(_on_WagonDetector_body_entered)
 
 func _on_WagonDetector_body_entered(body):
-    if body.name == "Player":
-        body.register_wagon(get_instance_id())
+	if body.name == "Player":
+		body.register_wagon(get_instance_id())
